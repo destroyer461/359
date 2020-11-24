6540a8b697cfc2158f454eb2954a9b0f0be8e360
destroyer461 <74886866+destroyer461@users.noreply.github.com>
2020-11-24 20:52:07 +0530

    Add files via upload

---
+++ CALCULATOR.js
@@ -0,0 +1,18 @@
+function add(){
+    var x=Number(document.getElementById("input").value)+Number(document.getElementById("input2").value)
+    window.alert(x);
+
+}
+function subtract(){
+    var y=Number(document.getElementById("input").value)-Number(document.getElementById("input2").value)
+    window.alert(y);
+
+}
+function divide(){
+    var z= Number(document.getElementById("input").value)/Number(document.getElementById("input2").value)
+    window.alert(z)
+}
+function multiply(){
+    var a= Number(document.getElementById("input").value)*Number(document.getElementById("input2").value)
+    window.alert(a)
+}
